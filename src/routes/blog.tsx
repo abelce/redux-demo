2d7c78035a23515f369cea6b10480b3fc674c601
tzx <tzx@tzx.local>
2018-09-16 10:49:56 +0800

    配置webpack将blog模块单独打出来

---
+++ src/routes/blog.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Switch, Route, } from 'react-router-dom';
+import { Layout } from 'antd'
+import Artice from '../components/article';
+import ArticleDetail from '../components/article/detail';
+import EditContainer from '../components/article/editContainer';
+import * as style from './style.scss';
+import Modals from '../components/common/modals';
+
+class Routes extends React.Component {
+  render() {
+      return(
+          <Layout className={style.layout}>
+              <Layout.Content className={style.content}>
+                <Switch>
+                  <Route exact path="/" component={Artice}/>
+                  <Route exact path="/article" component={Artice}/>
+                  <Route exact path="/article/:id" component={ArticleDetail}/>
+                  <Route exact path="/article/edit/:id" component={EditContainer}/>        
+                </Switch>
+              </Layout.Content>
+            <Modals/>                     
+          </Layout>
+      )
+  } 
+}
+
+export default Routes;
