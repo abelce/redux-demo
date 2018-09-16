bfacc9d3c47ff90273c1d3ef57a7a1c9e6857c0d
tzx <tzx@tzx.local>
2018-09-17 01:20:05 +0800

    注释掉删除按钮

--- src/components/article/item.tsx
+++ src/components/article/item.tsx
@@ -43,7 +43,7 @@ const Item = ({ article, onDelete }: itemType) => {
         <RenderMarked markdowncontent={article.attributes.markdowncontent}/>
       </div>
       <footer>
-        <a style={{display: isApp() ? 'inline-block' : 'none'}} onClick={() => onDelete(article.id)}>删除</a>
+        {/* <a style={{display: isApp() ? 'inline-block' : 'none'}} onClick={() => onDelete(article.id)}>删除</a> */}
         <Link to={`/article/${article.id}`}>
           <Button size="small" type="dashed">查看全文</Button>
         </Link>
