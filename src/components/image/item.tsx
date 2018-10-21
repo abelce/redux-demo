184cb898b3f0075b0c8cc922246baa4cf267986e
tzx <tzx@tzx.local>
2018-10-21 22:28:30 +0800

    添加prettier

--- src/components/image/item.tsx
+++ src/components/image/item.tsx
@@ -1,52 +1,67 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import * as style from './style.scss';
 import { Image } from '../../domain';
 import Img from 'react-lazy-imager';
 import cn from 'classnames';
 
 interface PItem {
-  image: Image
+  image: Image;
 }
 
 interface SItem {
-  loaded: boolean
+  loaded: boolean;
 }
 
 class Item extends React.Component<PItem, SItem> {
-
   static defaultProps = {
-    image: {}
-  }
+    image: {},
+  };
 
   state = {
     loaded: false,
-  }
+  };
 
-  ref: any
+  ref: any;
 
   setRef = (r: any) => {
     this.ref = r;
-  }
+  };
 
   onLoad = () => {
     this.setState({
       loaded: true,
-    })
-  }
+    });
+  };
 
   render() {
-    const {attributes: {url, width, height, svgurl }} = this.props.image;
-    const { loaded } = this.state
+    const {
+      attributes: { url, width, height, svgurl },
+    } = this.props.image;
+    const { loaded } = this.state;
 
     return (
-      <div ref={this.setRef}
-      className={style.imageContainer}
-      style={{width: `${width * 200/ height}px`, flexGrow: width * 200 / height}}>
-        <Img src={url} thumbSrc={svgurl} onLoad={this.onLoad} className={cn({'unload': !loaded})}/>
-        <div className="placeholder" style={{paddingBottom: `${height / width * 100}%`, display: loaded ? 'none' : 'block'}}></div>
+      <div
+        ref={this.setRef}
+        className={style.imageContainer}
+        style={{
+          width: `${(width * 200) / height}px`,
+          flexGrow: (width * 200) / height,
+        }}>
+        <Img
+          src={url}
+          thumbSrc={svgurl}
+          onLoad={this.onLoad}
+          className={cn({ unload: !loaded })}
+        />
+        <div
+          className="placeholder"
+          style={{
+            paddingBottom: `${(height / width) * 100}%`,
+            display: loaded ? 'none' : 'block',
+          }}
+        />
       </div>
-    )
+    );
   }
 }
 
