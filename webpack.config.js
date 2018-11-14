db504e6c9242edbe2e9f92f2a02088723ecab915
tzx <tzx@tzx.local>
2018-11-14 23:58:21 +0800

    add electron

--- webpack.config.js
+++ webpack.config.js
@@ -1,177 +1,190 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 const fs = require('fs');
 
 const loading = {
-  html: fs.readFileSync(path.join(__dirname, './src/assets/loading/index.html')),
-  css: '<style>' + fs.readFileSync(path.join(__dirname, './src/assets/loading/style.css')) + '</style>',
-  js: '<script>' + fs.readFileSync(path.join(__dirname, './src/assets/loading/loading.js')) + '</script>'
-}
+  html: fs.readFileSync(
+    path.join(__dirname, './src/assets/loading/index.html')
+  ),
+  css:
+    '<style>' +
+    fs.readFileSync(path.join(__dirname, './src/assets/loading/style.css')) +
+    '</style>',
+  js:
+    '<script>' +
+    fs.readFileSync(path.join(__dirname, './src/assets/loading/loading.js')) +
+    '</script>',
+};
 
 const hostMap = {
   development: '"http://localhost:3001"',
 };
 
 const config = {
-  mode: isDev() ? "development" : "production",
-  devtool: isDev() ? "cheap-module-eval-source-map" : false,
+  mode: isDev() ? 'development' : 'production',
+  devtool: isDev() ? 'cheap-module-eval-source-map' : false,
   devServer: {
     disableHostCheck: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     hot: true,
     port: 3010,
-    contentBase: __dirname + "/dist",
+    contentBase: __dirname + '/dist',
     historyApiFallback: true,
   },
   entry: {
     vendor: [
-      "react",
-      "react-dom",
-      "react-router-dom",
-      "lodash",
-      "moment",
-      "redux",
-      "react-redux",
-      "axios",
-      "redux-actions",
-      "redux-saga",
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'lodash',
+      'moment',
+      'redux',
+      'react-redux',
+      'axios',
+      'redux-actions',
+      'redux-saga',
     ],
-    app: [
-      __dirname + "/src/app.tsx",
-    ],
-    po: ["./src/assets/i18ns/zh_CN.po", "./src/assets/i18ns/en_US.po"],
-    blog: __dirname + "/src/blog.tsx",
+    app: [__dirname + '/src/app.tsx'],
+    po: ['./src/assets/i18ns/zh_CN.po', './src/assets/i18ns/en_US.po'],
+    blog: __dirname + '/src/blog.tsx',
+    blog: __dirname + '/src/electron.tsx',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: `${process.env.PUBLIC || "/"}`,
-    filename: isDev() ? "[name].[hash].js" : "[name].[chunkhash].js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: `${process.env.PUBLIC || '/'}`,
+    filename: isDev() ? '[name].[hash].js' : '[name].[chunkhash].js',
   },
   module: {
     rules: [
       {
         test: /\/src\/assets\/script\/noise.js/,
-        loader: "expose-loader?noise!exports-loader!imports-loader",
+        loader: 'expose-loader?noise!exports-loader!imports-loader',
       },
       {
         test: /zh_CN.po$/,
-        loader: "expose-loader?zh_CN!json-loader!po-loader",
+        loader: 'expose-loader?zh_CN!json-loader!po-loader',
       },
       {
         test: /en_US.po$/,
-        loader: "expose-loader?en_US!json-loader!po-loader",
+        loader: 'expose-loader?en_US!json-loader!po-loader',
       },
       {
         test: /axios$/,
-        loader: "expose-loader?axios",
+        loader: 'expose-loader?axios',
       },
       {
         test: /moment$/,
-        loader: "expose-loader?moment",
+        loader: 'expose-loader?moment',
       },
       {
         test: /\.jsx$|\.js$|\.tsx$|\.ts$/,
         include: /src/,
         exclude: [/node_modules/, /src\/assets/],
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg|pdf|jpg)$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          name: "[path][name].[hash].[ext]",
+          name: '[path][name].[hash].[ext]',
           limit: 8192,
-          context: "src",
-          fallback: "file-loader",
+          context: 'src',
+          fallback: 'file-loader',
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
         loader: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "typings-for-css-modules-loader",
+            loader: 'typings-for-css-modules-loader',
             options: {
               modules: true,
               namedExport: true,
               camelCase: true,
               minimize: true,
-              localIdentName: "[local]_[hash:base64:5]",
+              localIdentName: '[local]_[hash:base64:5]',
             },
           },
-          "resolve-url-loader",
+          'resolve-url-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
-              ident: "postcss",
+              ident: 'postcss',
               plugins: function() {
-                return [require("autoprefixer")];
+                return [require('autoprefixer')];
               },
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      excludeChunks: ["blog"],
-      template: __dirname + "/src/assets/index.ejs",
+      excludeChunks: ['blog', 'electron'],
+      template: __dirname + '/src/assets/index.ejs',
     }),
     new HtmlWebpackPlugin({
-      excludeChunks: ["app"],
-      filename: "blog.html",
-      template: __dirname + "/src/assets/blog.ejs",
+      excludeChunks: ['app', 'electron'],
+      filename: 'blog.html',
+      template: __dirname + '/src/assets/blog.ejs',
+      loading,
+    }),
+    new HtmlWebpackPlugin({
+      excludeChunks: ['app', 'blog'],
+      filename: 'electron.html',
+      template: __dirname + '/src/assets/electron.ejs',
       loading,
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
     new webpack.DefinePlugin({
       __ENV__: JSON.stringify(process.env.NODE_ENV),
       __: function(k) {
         return k;
       },
-      _pms_host: hostMap[process.env.NODE_ENV] || "",
+      _pms_host: hostMap[process.env.NODE_ENV] || '',
     }),
     new webpack.HashedModuleIdsPlugin(),
   ],
   resolve: {
-    modules: ["src", "node_modules"],
-    extensions: [".js", ".tsx", ".ts", ".scss"],
+    modules: ['src', 'node_modules'],
+    extensions: ['.js', '.tsx', '.ts', '.scss'],
   },
   optimization: {
     minimize: isDev() ? false : true,
     // runtimeChunk: true,
     splitChunks: {
-      chunks: "initial",
+      chunks: 'initial',
       minSize: 30000,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
+      automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
         common: {
           test: /.js$/,
-          name: "common",   // 名字，设置为true，表示根据模块和缓存组自动生成，
-          chunks: "initial",  // initial: 初始块，all: 所有块，async：按需加载
+          name: 'common', // 名字，设置为true，表示根据模块和缓存组自动生成，
+          chunks: 'initial', // initial: 初始块，all: 所有块，async：按需加载
           priority: 2,
-          minChunks: 2,   // 最小引用次数
-          reuseExistingChunk: true,  // 是否使用已经存在的chunk, 前提是代码没有变化，
+          minChunks: 2, // 最小引用次数
+          reuseExistingChunk: true, // 是否使用已经存在的chunk, 前提是代码没有变化，
         },
         styles: {
-          name: "styles",
+          name: 'styles',
           test: /\.css$/,
-          chunks: "all",
+          chunks: 'all',
           enforce: true,
           priority: 20,
         },
@@ -187,5 +200,5 @@ if (isDev()) {
 module.exports = config;
 
 function isDev() {
-  return process.env.NODE_ENV === "development";
+  return process.env.NODE_ENV === 'development';
 }
